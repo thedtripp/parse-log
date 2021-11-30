e763812bf141058550c8b00c8cf03fb554624cfe
thedtripp <thedtripp@gmail.com>
2021-11-29 21:56:36 -0800

    deleted commented out code

--- test_get_cookies.py
+++ test_get_cookies.py
@@ -87,16 +87,6 @@ class TestCookieGetter(unittest.TestCase):
                 ("fbcn5UAVanZf6UtG", datetime.date(2018, 12, 8)),
                 ("4sMM2LxV07bPJzwf", datetime.date(2018, 12, 7)),
             ],
-            # [
-            #     "AtY0laUfhglK3lC7,2018-12-09T14:19:00+00:00",
-            #     "SAZuXPGUrfbcn5UA,2018-12-09T10:13:00+00:00",
-            #     "5UAVanZf6UtGyKVS,2018-12-09T07:25:00+00:00",
-            #     "AtY0laUfhglK3lC7,2018-12-09T06:19:00+00:00",
-            #     "SAZuXPGUrfbcn5UA,2018-12-08T22:03:00+00:00",
-            #     "4sMM2LxV07bPJzwf,2018-12-08T21:30:00+00:00",
-            #     "fbcn5UAVanZf6UtG,2018-12-08T09:30:00+00:00",
-            #     "4sMM2LxV07bPJzwf,2018-12-07T23:30:00+00:00",
-            # ],
         )
         with self.assertRaises(SystemExit):
             self.csv_file_reader.read_file_to_list("./test_files/empty_file.txt")
